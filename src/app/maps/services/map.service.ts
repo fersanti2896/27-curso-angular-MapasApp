f908e33a728d27cb4f8b2e183345807c6c08e35a
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-18 00:41:46 -0500

    Agregando los marcadores y marcador principal al mapa con map.service.ts

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,12 +1,14 @@
-import { LngLatLike } from './../../../../node_modules/@types/mapbox-gl/index.d';
+import { LngLatBounds, LngLatLike } from 'mapbox-gl';
 import { Injectable } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { Map, Marker, Popup } from 'mapbox-gl';
+import { Feature } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
-  private map: Map | undefined;
+  private map    : Map | undefined;
+  private markers: Marker[] = [];
 
   get isMapReady() {
     return !!this.map;
@@ -24,5 +26,43 @@ export class MapService {
       zoom: 14, 
       center: coords
     })
+  }
+
+  createMarkersFromPlaces( places: Feature[], userLocation: [number, number] ) {
+    if( !this.map ) throw Error('Mapa no inicializado');
+
+    /* Purgamos los marcadores en el mapa */
+    this.markers.forEach( marker => marker.remove() );
+
+    const newMarkers = [];
+
+    for( const place of places) {
+      const [lng, lat] = place.center;
+      const popup = new Popup()
+          .setHTML(`
+            <h6>${ place.text }</h6>
+            <span>${ place.place_name }</span>
+          `);
+      const newMarker = new Marker()
+          .setLngLat([lng, lat])
+          .setPopup( popup )
+          .addTo( this.map );    
+          
+      newMarkers.push( newMarker );
+    }
+
+    this.markers = newMarkers;
+
+    if( places.length === 0 ) return;
+
+    /* Zoom inicial - Limites del mapa */
+    const bounds = new LngLatBounds();
+    
+    newMarkers.forEach( marker => bounds.extend( marker.getLngLat() ) );
+    bounds.extend(userLocation);
+
+    this.map.fitBounds(bounds, {
+      padding: 200  
+    });
   }
 }
--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PlacesApiClient } from '../api';
 import { PlacesRespone, Feature } from '../interfaces/places';
+import { MapService } from './map.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,8 @@ export class PlacesService {
     return !!this.useLocation;
   }
 
-  constructor(private placesApi: PlacesApiClient) {
+  constructor(private placesApi: PlacesApiClient,
+              private mapService: MapService) {
     this.getUserLocation();
   }
 
@@ -52,10 +53,10 @@ export class PlacesService {
       params: {
         proximity: this.useLocation.join(',')
       }
-    })
-        .subscribe( resp => {
-          this.isLoadingPlaces = false;
-          this.places          = resp.features;
-        });
+    }).subscribe( resp => {
+        this.isLoadingPlaces = false;
+        this.places          = resp.features;
+        this.mapService.createMarkersFromPlaces(this.places, this.useLocation!);
+      });
   }
 }
